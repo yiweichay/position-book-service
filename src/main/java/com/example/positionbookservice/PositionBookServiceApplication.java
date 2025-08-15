4c5b6c64588f63164b046da6716910f97e81d5b5
yiweichay <ywc19@ic.ac.uk>
2025-08-15 17:53:07 +0100

    Initial commit

---
+++ src/main/java/com/example/positionbookservice/PositionBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.positionbookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionBookServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PositionBookServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/positionbookservice/PositionBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.positionbookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionBookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
