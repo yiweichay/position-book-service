71b5afca5e82dfabf0a3401d5b301d3fbab7b9e2
yiweichay <ywc19@ic.ac.uk>
2025-08-16 16:11:25 +0100

    Added cors config

--- src/main/java/com/example/positionbookservice/entity/Positions.java
+++ src/main/java/com/example/positionbookservice/entity/Positions.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -12,5 +13,5 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Positions {
-    private List<Position> positions;
+    private List<Position> positions = new ArrayList<>();
 }
